30800b4119e0009c49bb61ae67eb93c44c0a7d27
mickael gomes <mickael gomes>
2017-02-03 18:07:50 +0000

    Statefull country code input added and working

--- client/components/auth/signUpForm/signUpForm.jsx
+++ client/components/auth/signUpForm/signUpForm.jsx
@@ -1,8 +1,10 @@
 const React = require('react')
 const PhoneNumberBox = require('./../phoneNumberBox/phoneNumberBox')
+const CountryCodeBox = require('./../countryCodeBox/countryCodeBox')
 
 const SignUpForm = () => (
   <div>
+    <CountryCodeBox />
     <PhoneNumberBox />
   </div>
 )
