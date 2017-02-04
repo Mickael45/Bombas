0dac45aca4e70b1b03d0524ef8487b79894a5ada
mickael gomes <mickael gomes>
2017-02-04 15:48:00 +0000

    Empty invoice form added to page

---
+++ client/components/profile/invoiceInfoTile/invoiceInfoTile.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const InvoiceInfoTile = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Invoice</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = InvoiceInfoTile
