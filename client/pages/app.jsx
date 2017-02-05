2f9843fa9e35666aaa946eefbbb6a483bd5889de
mickael gomes <mickachoux12@gmail.com>
2017-02-05 23:29:10 +0000

    Logout functionnality added and working

--- client/pages/app.jsx
+++ client/pages/app.jsx
@@ -1,12 +1,12 @@
 const React = require('react')
-const NavBar = require('./../components/navBar')
+const NavBarContainer = require('./../containers/navBarContainer')
 const AppContainer = require('./../containers/appContainer')
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <NavBar />
+        <NavBarContainer />
         <AppContainer>
           {this.props.children}
         </AppContainer>
