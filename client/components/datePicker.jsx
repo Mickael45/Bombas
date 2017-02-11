c606ccee004e853129e6b5de10737744bcffcb52
mickael gomes <mickachoux12@gmail.com>
2017-02-11 17:34:59 +0000

    datePicker callbacks and values passed to xml component

--- client/components/datePicker.jsx
+++ client/components/datePicker.jsx
@@ -1,36 +1,32 @@
 const React = require('react')
 const DP = require('react-bootstrap-date-picker')
-const { ControlLabel, HelpBlock, FormGroup, Col } = require('react-bootstrap')
+const { func, string } = React.PropTypes
+const { FormGroup, Col } = require('react-bootstrap')
 
 const DatePicker = React.createClass({
-  getInitialState () {
-    var value = new Date().toISOString()
-    return {
-      beginning: value,
-      ending: value
-    }
-  },
-  handleBegginingChange (value) {
-    this.setState({ beginning: value })
-  },
-  handleEndingChange (value) {
-    this.setState({ ending: value })
+  propTypes: {
+    beginning: string,
+    ending: string,
+    handleBeginningChange: func,
+    handleEndingChange: func
   },
   render () {
     return (
       <div>
         <Col md={3} mdOffset={3} xs={3} xsOffset={3}>
           <FormGroup>
-            <ControlLabel>Desde</ControlLabel>
-            <DP id='example-datepicker' value={this.state.beginning} onChange={this.handleBegginingChange} />
-            <HelpBlock>Help</HelpBlock>
+            <p className='input-title'>
+              Desde
+            </p>
+            <DP id='example-datepicker' value={this.props.beginning} onChange={this.props.handleBeginingChange} />
           </FormGroup>
         </Col>
         <Col md={3} xs={3}>
           <FormGroup>
-            <ControlLabel>Até</ControlLabel>
-            <DP id='example-datepicker' value={this.state.ending} onChange={this.handleChandleEndingChangehange} />
-            <HelpBlock>Help</HelpBlock>
+            <p className='input-title'>
+              Até
+            </p>
+            <DP id='example-datepicker' value={this.props.ending} onChange={this.props.handleEndingChange} />
           </FormGroup>
         </Col>
       </div>
--- client/components/xml.jsx
+++ client/components/xml.jsx
@@ -1,12 +1,33 @@
 const React = require('react')
 const DatePicker = require('./datePicker')
 const XmlButton = require('./xmlButton')
+const { func } = React.PropTypes
 
 const Xml = React.createClass({
+  propTypes: {
+    createXmlFile: func
+  },
+  getInitialState () {
+    var value = new Date().toISOString()
+    return {
+      beginning: value,
+      ending: value
+    }
+  },
+  handleBeginningChange (value) {
+    this.setState({ beginning: value })
+  },
+  handleEndingChange (value) {
+    this.setState({ ending: value })
+  },
   render () {
     return (
       <div>
-        <DatePicker />
+        <DatePicker
+          beginning={this.state.beginning}
+          ending={this.state.ending}
+          handleBeginningChange={this.handleBeginningChange}
+          handleEndingChange={this.handleEndingChange} />
         <XmlButton />
       </div>
     )
